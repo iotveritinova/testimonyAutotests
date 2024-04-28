af4a7a5a9f4336aca595bff48bc886d87e248923
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-04-29 00:15:57 +0400

    logback fix

--- testimonyAutotestUI/src/test/java/ru/neoflex/PageNavigationTest.java
+++ testimonyAutotestUI/src/test/java/ru/neoflex/PageNavigationTest.java
@@ -2,15 +2,20 @@ package ru.neoflex;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PageNavigationTest extends TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @Test
     public void FromMainToSendAndBack() {
+        logger.info("Start FromMainToSendAndBack");
         applicationManager.getMainPage().clickSend();
         Assertions.assertEquals(applicationManager.getSendPage().getHeaderText(), "Передача показаний");
         applicationManager.getSendPage().clickBackButton();
         Assertions.assertEquals(applicationManager.getMainPage().getHeaderText(), "Neo ЖКХ");
+        logger.info("End FromMainToSendAndBack");
     }
 
     @Test
