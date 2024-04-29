c8989054db8be3ca2b77ce417b05677f2b40dbb0
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-04-29 23:12:48 +0400

    add alllure reporting

--- testimonyAutotestUI/src/test/java/ru/neoflex/PageNavigationTest.java
+++ testimonyAutotestUI/src/test/java/ru/neoflex/PageNavigationTest.java
@@ -1,13 +1,28 @@
 package ru.neoflex;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 
 public class PageNavigationTest extends TestBase {
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
+    //add allure
+    //@Step - данная аннотация используется для обозначения тестового шага
+    //@DisplayName - данная аннотация используется для того, чтобы задать название тестовому шагу
+    //@Description - данная аннотация используется для того, чтобы задать описание к тестовому шагу
+    //Для того, чтобы после выполнения теста сформировать отчёт и открыть его в браузере,
+    // необходимо выполнить следующие действия:
+    // 1) Открыть Terminal в IntelliJ IDEA
+    // 2) Ввести команду: allure generate --clean
+    // 3) Ввести команду: allure serve
+    // 4) После выполнения команды allure serve откроется окно браузера
+    // с отчётом по запущенным тестам
+    @Step
+    @DisplayName("Navigation test")
+    @Description("Navigation test description")
     @Test
     public void FromMainToSendAndBack() {
         logger.info("Start FromMainToSendAndBack");
