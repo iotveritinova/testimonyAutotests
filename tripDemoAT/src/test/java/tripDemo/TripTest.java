75e5ce7407d0a636ac27f9e3ef3bda9f014e675d
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-05-06 02:21:43 +0400

    add & apply ApiHelper

--- tripDemoAT/src/test/java/tripDemo/TripTest.java
+++ tripDemoAT/src/test/java/tripDemo/TripTest.java
@@ -2,7 +2,9 @@ package tripDemo;
 
 import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomUtils;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import tripDemo.api.ApiHelper;
 import tripDemo.dictionaries.IPathEnum;
 import tripDemo.dictionaries.TripPathEnum;
 import tripDemo.generator.JsonGenerator;
@@ -13,95 +15,60 @@ import tripDemo.service.ConfigQA;
 import java.util.ArrayList;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
 public class TripTest {
 
-    private static Map<IPathEnum, String> serviceDataMap;
+    private Map<IPathEnum, String> serviceDataMap;
+    private Trip createTrip, putTrip;
 
     @BeforeClass
-    public static void init(){
-        serviceDataMap= ConfigQA.getInstance().getServiceDataMap();
-    }
-
-    @Test
-    public void createTrip() {
-        Trip trip = new Trip.Builder()
+    public void init() {
+        serviceDataMap = ConfigQA.getInstance().getServiceDataMap();
+        createTrip = new Trip.Builder()
                 .withRandomMainInfo(1)
                 .withPassengers(new ArrayList<>() {{
                     for (int i = 0; i < RandomUtils.nextInt(1, 3); i++) {
                         add(new Passenger.Builder().withRandomCompletely().build());
                     }
                 }}).build();
-        String body = JsonGenerator.toJsonString(trip);
-        Response response = given().log().all(true)
-                .contentType("application/json")
-                .accept("application/json")
-                .body(body)
-                .when()
-                .post(serviceDataMap.get(TripPathEnum.CREATE_TRIP))
-                .thenReturn();
-        System.out.println(response.getBody().prettyPrint());
-        /*
-        Trip tripResult = given().log().all(true)
-                .contentType("application/json")
-                .accept("application/json")
-                .body(trip)
-                .when()
-                .post("http://localhost:8080/trip/createTrip")
-                .as(Trip.class);
-        * */
-    }
 
-    @Test
-    public void putTrip() {
-        Trip trip = new Trip.Builder()
+        putTrip = new Trip.Builder()
                 //Пока что нужно установить id существующей модели в базе
                 .withId(6L)
                 .withRandomMainInfo(1)
                 .withPassengers(new ArrayList<>() {{
-                    for (int i = 0; i < RandomUtils.nextInt(1, 3); i++) {
-                        add(new Passenger.Builder().withRandomCompletely().build());
-                    }
+                    add(new Passenger.Builder()
+                            .withId(10L)
+                            .withRandomCompletely().build());
                 }}).build();
-        String body = JsonGenerator.toJsonString(trip);
-        Response response = given()
-                .log().all(true)
-                .contentType("application/json")
-                .accept("application/json")
-                .body(body )
-                .when()
-                .put(serviceDataMap.get(TripPathEnum.PUT_TRIP))
-                .thenReturn();
-
-        System.out.println(response.getBody().prettyPrint());
     }
 
     @Test
-    public void deleteTrip() {
-        Response response = given()
-                .log().all(true)
-                .contentType("application/json")
-                .accept("application/json")
-                .when()
-                .delete(serviceDataMap.get(TripPathEnum.DELETE_TRIP))
-                .thenReturn();
+    public void createTrip() {
+        String result = JsonGenerator.toJsonString(createTrip);
+        String path = serviceDataMap.get(TripPathEnum.CREATE_TRIP);
+        Response response = ApiHelper.post(path, result);
         System.out.println(response.getBody().prettyPrint());
     }
 
     @Test
     public void getTrip() {
-        given()
-                .log().all(true)
-                .contentType("application/json")
-                .accept("application/json")
-                .when()
-                .get(serviceDataMap.get(TripPathEnum.GET_TRIP))
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("townFrom", equalTo("Moscow"));
+        String path = serviceDataMap.get(TripPathEnum.GET_TRIP);
+        Response response = ApiHelper.get(path, 6);
+        System.out.println(response.getBody().prettyPrint());
     }
 
+    @Test
+    public void putTrip() {
+        String path = serviceDataMap.get(TripPathEnum.PUT_TRIP);
+        String result = JsonGenerator.toJsonString(putTrip);
+        Response response = ApiHelper.put(path, result);
+        System.out.println(response.getBody().prettyPrint());
+    }
+
+    @Test
+    public void deleteTrip() {
+        String path = serviceDataMap.get(TripPathEnum.DELETE_TRIP);
+        Response response = ApiHelper.delete(path, 6);
+        System.out.println(response.getBody().prettyPrint());
+    }
 }
