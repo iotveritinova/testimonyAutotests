c82a61092b945ea03c32033fe0c31d25c0481318
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-05-11 23:26:25 +0400

    фин коммит для задания

--- tripDemoAT/src/main/java/tripDemo/db/BaseConnection.java
+++ tripDemoAT/src/main/java/tripDemo/db/BaseConnection.java
@@ -6,25 +6,18 @@ import tripDemo.service.ConfigQA;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Map;
 
 public class BaseConnection {
     private static Connection connection;
-
     //4. Создать класс BaseConnection, в котором будут два статических метода getConnection и closeConnection.
     // Метод getConnection() должен принимать параметр ServiceEnum, после этого проверять следующее:
     // Если для данного сервиса уже есть созданная сессия, то вернуть ее, иначе создать новое подключение.
 
-    public static Connection getConnection(ServiceEnum enumTrip) throws SQLException {
-        //переменные перенести в ServiceEnum и добавить его в параметр метода
-        //System.out.println(dbConnectionDataMap.get(ServiceEnum.TRIP));
-       // ConnectionProperties connectionProperties = dbConnectionDataMap.get(enumTrip);
-        String url =//connectionProperties.getUrl();//
-                 "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "148192";
+    public static Connection getConnection(ServiceEnum serviceEnum) throws SQLException {
+        ConnectionProperties connectionProperties = ConfigQA.getInstance()
+                .getDbConnectionDataMap().get(serviceEnum);
         if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(connectionProperties.getUrl(), connectionProperties.getUser(), connectionProperties.getPassword());
         }
         return connection;
     }
--- tripDemoAT/src/test/java/tripDemo/DbTest.java
+++ tripDemoAT/src/test/java/tripDemo/DbTest.java
@@ -1,21 +1,15 @@
 package tripDemo;
 
 import org.testng.annotations.Test;
-import tripDemo.db.ConnectionProperties;
-import tripDemo.dictionaries.ServiceEnum;
 import tripDemo.model.Passenger;
 import tripDemo.model.Trip;
-import tripDemo.service.ConfigQA;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Map;
 
 import static tripDemo.db.PassengerRepository.getPassengerById;
 import static tripDemo.db.TripRepository.getTripById;
 
-public class dbTest {
-    private static final Map<ServiceEnum, ConnectionProperties> dbConnectionDataMap = ConfigQA.getInstance().getDbConnectionDataMap();
+public class DbTest {
 
     @Test
     public void DBConnectAndSelect() throws SQLException {
@@ -23,10 +17,5 @@ public class dbTest {
         Trip trip = getTripById(3);
         System.out.println(passenger);
         System.out.println(trip);
-    }
-
-    @Test
-    public void DBConTest() {
-        System.out.println(dbConnectionDataMap.get(ServiceEnum.TRIP));
     }
 }
