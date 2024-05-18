90f95f7ff64938ee46a112a9b83a57b48679301d
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2024-05-18 21:51:14 +0400

    add Compant for db

---
+++ tripDemoAT/src/main/java/tripDemo/model/Company.java
@@ -0,0 +1,21 @@
+package tripDemo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "company")
+@Data
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String name;
+
+    @Column(name = "open_date")
+    private LocalDate openDate;
+}
